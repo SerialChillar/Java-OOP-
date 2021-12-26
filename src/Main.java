51cdae4898c4768b5c6d9dfb7b39491549c1c1a2
SerialChillar <afridirahman605@gmail.com>
2021-12-26 15:18:26 +0600

    constructor and this

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        Waiter waiter1=new Waiter();
-        waiter1.name="Superman ";
-        waiter1.age=50;//C201045
-        waiter1.sex="Male";
-        waiter1.date="26-12-2021 ";
-        waiter1.salary=10000;
-        waiter1.hsc=3.50;
+        Waiter waiter1=new Waiter("Superman","Male","26-12-2021",50,10000,3.50);
         waiter1.detailsPrint(waiter1.name,waiter1.age,waiter1.sex,waiter1.date,waiter1.salary,waiter1.hsc);
-
-    }
+    }//C201045
 }
--- src/Waiter.java
+++ src/Waiter.java
@@ -2,7 +2,18 @@ public class Waiter {
     String name,sex,date;
     int age;
     double salary,hsc;
-    public void detailsPrint(String name,int age, String sex, String date,double salary,double hsc){
+    public Waiter(String name, String sex, String date, int age,double salary,double hsc) {
+        this.name = name;
+        this.sex = sex;
+        this.date = date;
+        this.age = age;
+        this.salary=salary;
+        this.hsc=hsc;
+
+    }
+
+    public void detailsPrint(String name, int age, String sex, String date,double salary,double hsc){
         System.out.println(name+""+age+""+sex+""+date+""+salary+""+hsc);
-    }//C201045
+    }
+
 }
