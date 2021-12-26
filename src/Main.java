8ad632407143b99863e766f6c0d37fe30e6eb398
SerialChillar <afridirahman605@gmail.com>
2021-12-26 15:39:51 +0600

    Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        Waiter waiter1=new Waiter("Superman","Male","26-12-2021",50,10000,3.50);
-        waiter1.detailsPrint(waiter1.name,waiter1.age,waiter1.sex,waiter1.date,waiter1.salary,waiter1.hsc);
+        Waiter waiter1=new Waiter("Mirjafor","Male","26-12-2021",50,10000,3.50);
+        waiter1.detailsPrint(waiter1.getName(),waiter1.getAge(),waiter1.getSex(),waiter1.getDate(),waiter1.getSalary(),waiter1.getHsc());
     }//C201045
+
 }
--- src/Waiter.java
+++ src/Waiter.java
@@ -1,19 +1,66 @@
 public class Waiter {
-    String name,sex,date;
-    int age;
-    double salary,hsc;
+    private String name, sex, date;
+    private int age;
+    private double salary, hsc;
+
     public Waiter(String name, String sex, String date, int age,double salary,double hsc) {
         this.name = name;
         this.sex = sex;
         this.date = date;
         this.age = age;
-        this.salary=salary;
-        this.hsc=hsc;
-
+        this.salary = salary;
+        this.hsc = hsc;
     }
 
-    public void detailsPrint(String name, int age, String sex, String date,double salary,double hsc){
-        System.out.println(name+""+age+""+sex+""+date+""+salary+""+hsc);
+    public void detailsPrint(String name, int age, String sex, String date, double salary, double hsc) {
+        System.out.println(name + "" + age + "" + sex + "" + date + "" + salary + "" + hsc);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getHsc() {
+        return hsc;
+    }
+
+    public void setHsc(double hsc) {
+        this.hsc = hsc;
+    }
 }
